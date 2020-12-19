76c51aec1b0c066cc696149ab5edea1ebad487b9
Elliott <eat2153@columbia.edu>
2020-12-18 21:15:27 -0500

    added tests

--- tests/test_geocenpy.py
+++ tests/test_geocenpy.py
@@ -1,5 +1,27 @@
 from geocenpy import __version__
 from geocenpy import geocenpy
+import geopandas as gpd
+import urllib.request
+import json
+import base64
+import pandas as pd
+import matplotlib.pyplot as plt
+import descartes
+import lxml
+import requests
+from requests.exceptions import HTTPError
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_list_geoparams():
+    geoparams_df = geocenpy.list_geoparams()
+    assert type(geoparams_df) == pandas.core.frame.DataFrame
+
+def test_get_geocen_df():
+    df = geocenpy.get_geocen_df(quality = "20m", year = "2010", area_type= "county")
+    assert df.shape == (3221, 7)
+
+def test_get_state_ids():
+    geoID = geocenpy.get_state_ids(state_initials = "OR")
+    assert type(geoID) == pandas.core.frame.DataFrame
